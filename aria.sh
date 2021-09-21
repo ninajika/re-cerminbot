2fd621ab4255de7d40d5cfafa5a6dca511cc84ac
Nekaru <private.nafi2016@gmail.com>
2021-09-21 15:58:58 +0700

    aria2: reoptimize
    - upgrade to 4.3.8 qbit
    - update user-agent
    - increase from 8 to 10
    - decreasee max tries from 20 to 15

--- aria.sh
+++ aria.sh
@@ -1,9 +1,9 @@
 aria2c --enable-rpc --check-certificate=false \
-   --max-connection-per-server=8 --rpc-max-request-size=512M \
+   --max-connection-per-server=10 --rpc-max-request-size=512M \
    --bt-stop-timeout=0 --min-split-size=10M --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=0 \
    --max-overall-upload-limit=1K --max-concurrent-downloads=15 --continue=true \
-   --peer-id-prefix=-qB4360- --user-agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 11_5_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36' --peer-agent=qBittorrent/4.3.6 \
+   --peer-id-prefix=-qB4380- --user-agent='Mozilla/5.0 (iPad; CPU OS 12_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Mobile/15E148 Safari/604.1' --peer-agent=qBittorrent/4.3.8 \
    --disk-cache=32M --bt-enable-lpd=true --seed-time=0 --max-file-not-found=0 \
-   --max-tries=20 --auto-file-renaming=true --reuse-uri=true --http-accept-gzip=true \
+   --max-tries=15 --auto-file-renaming=true --reuse-uri=true --http-accept-gzip=true \
    --content-disposition-default-utf8=true --netrc-path=/usr/src/app/.netrc
