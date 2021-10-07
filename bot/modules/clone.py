anasty17/mirror-leech-telegram-bot

Extract rar, zip and 7z splits with or without password -Other changes:  Download limit now by default in GB, no need to add unit anymore
Signed-off-by: anas <e.anastayyar@gmail.com>

bot/modules/clone.py
@@ -7,9 +7,10 @@ from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.mirror_utils.status_utils.clone_status import CloneStatus
 from bot import dispatcher, LOGGER, CLONE_LIMIT, STOP_DUPLICATE, download_dict, download_dict_lock, Interval
-from bot.helper.ext_utils.bot_utils import get_readable_file_size, check_limit
+from bot.helper.ext_utils.bot_utils import get_readable_file_size, new_thread
 
 
+@new_thread
 def cloneNode(update, context):
     args = update.message.text.split(" ", maxsplit=1)
     if len(args) > 1:
@@ -27,12 +28,12 @@ def cloneNode(update, context):
                 sendMarkup(msg3, context.bot, update, button)
                 return
         if CLONE_LIMIT is not None:
-            result = check_limit(size, CLONE_LIMIT)
-            if result:
-                msg2 = f'Failed, Clone limit is {CLONE_LIMIT}.\nYour File/Folder size is {get_readable_file_size(size)}.'
+            LOGGER.info('Checking File/Folder Size...')
+            if size > CLONE_LIMIT * 1024**3:
+                msg2 = f'Failed, Clone limit is {CLONE_LIMIT}GB.\nYour File/Folder size is {get_readable_file_size(size)}.'
                 sendMessage(msg2, context.bot, update)
                 return
-        if files < 15:
+        if files <= 10:
             msg = sendMessage(f"Cloning: <code>{link}</code>", context.bot, update)
             result, button = gd.clone(link)
             deleteMessage(context.bot, msg)

bot/modules/mirror.py
@@ -12,7 +12,6 @@ import shutil
 
 from telegram.ext import CommandHandler
 from telegram import InlineKeyboardMarkup
-from fnmatch import fnmatch
 
 from bot import Interval, INDEX_URL, BUTTON_FOUR_NAME, BUTTON_FOUR_URL, BUTTON_FIVE_NAME, BUTTON_FIVE_URL, \
                 BUTTON_SIX_NAME, BUTTON_SIX_URL, BLOCK_MEGA_FOLDER, BLOCK_MEGA_LINKS, VIEW_LINK, aria2, \
@@ -112,8 +111,9 @@ class MirrorListener(listeners.MirrorListeners):
                 if os.path.isdir(m_path):
                     for dirpath, subdir, files in os.walk(m_path, topdown=False):
                         for filee in files:
-                            suffixes = (".part1.rar", ".part01.rar", ".part001.rar", ".part0001.rar")
-                            if (filee.endswith(".rar") and "part" not in filee) or filee.endswith(suffixes):
+                            if re.search(r'\.part0*1.rar$', filee) or re.search(r'\.7z.0*1$', filee) \
+                               or (filee.endswith(".rar") and not re.search(r'\.part\d+.rar$', filee)) \
+                               or re.search(r'\.zip.0*1$', filee):
                                 m_path = os.path.join(dirpath, filee)
                                 if pswd is not None:
                                     result = subprocess.run(["7z", "x", f"-p{pswd}", m_path, f"-o{dirpath}"])
@@ -123,7 +123,8 @@ class MirrorListener(listeners.MirrorListeners):
                                     LOGGER.warning('Unable to extract archive!')
                                 break
                         for filee in files:
-                            if filee.endswith(".rar") or fnmatch(filee, "*.r[0-9]") or fnmatch(filee, "*.r[0-9]*"):
+                            if filee.endswith(".rar") or re.search(r'\.r\d+$', filee) \
+                               or re.search(r'\.7z.\d+$', filee) or re.search(r'\.zip.\d+$', filee):
                                 del_path = os.path.join(dirpath, filee)
                                 os.remove(del_path)
                     path = f'{DOWNLOAD_DIR}{self.uid}/{name}'
@@ -434,9 +435,9 @@ def _mirror(bot, update, isTar=False, extract=False, isZip=False, isQbit=False, 
             sendMessage(res, bot, update)
             return
         if TAR_UNZIP_LIMIT is not None:
-            result = bot_utils.check_limit(size, TAR_UNZIP_LIMIT)
-            if result:
-                msg = f'Failed, Tar/Unzip limit is {TAR_UNZIP_LIMIT}.\nYour File/Folder size is {get_readable_file_size(size)}.'
+            LOGGER.info('Checking File/Folder Size...')
+            if size > TAR_UNZIP_LIMIT * 1024**3:
+                msg = f'Failed, Tar/Unzip limit is {TAR_UNZIP_LIMIT}GB.\nYour File/Folder size is {get_readable_file_size(size)}.'
                 sendMessage(msg, bot, update)
                 return
         LOGGER.info(f"Download Name: {name}")

ninajika/re-cerminbot

bot/modules/clone.py
@@ -5,7 +5,7 @@ from telegram.ext import CommandHandler
 
 from bot import (CLONE_LIMIT, LOGGER, STOP_DUPLICATE, Interval, dispatcher,
                  download_dict, download_dict_lock)
-from bot.helper.ext_utils.bot_utils import check_limit, get_readable_file_size
+from bot.helper.ext_utils.bot_utils import new_thread, get_readable_file_size
 from bot.helper.mirror_utils.status_utils.clone_status import CloneStatus
 from bot.helper.mirror_utils.upload_utils import gdriveTools
 from bot.helper.telegram_helper.bot_commands import BotCommands
@@ -13,6 +13,7 @@ from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import *
 
 
+@new_thread
 def cloneNode(update, context):
     args = update.message.text.split(" ", maxsplit=1)
     if len(args) > 1:
@@ -30,12 +31,12 @@ def cloneNode(update, context):
                 sendMarkup(msg3, context.bot, update, button)
                 return
         if CLONE_LIMIT is not None:
-            result = check_limit(size, CLONE_LIMIT)
-            if result:
+            LOGGER.info('Memeriksa Ukuran File/Folder...')
+            if size > CLONE_LIMIT * 1024**3:
                 msg2 = f'Gagal, batas klon adalah {CLONE_LIMIT}.\nUkuran file/folder Anda {get_readable_file_size(size)}.'
                 sendMessage(msg2, context.bot, update)
                 return
-        if files < 15:
+        if files < 10:
             msg = sendMessage(f"Kloning: <code>{link}</code>", context.bot, update)
             result, button = gd.clone(link)
             deleteMessage(context.bot, msg)

bot/modules/mirror.py
@@ -8,7 +8,6 @@ import subprocess
 import threading
 import time
 import urllib
-from fnmatch import fnmatch
 
 import requests
 from telegram import InlineKeyboardMarkup
@@ -149,15 +148,9 @@ class MirrorListener(listeners.MirrorListeners):
                 if os.path.isdir(m_path):
                     for dirpath, subdir, files in os.walk(m_path, topdown=False):
                         for file in files:
-                            suffixes = (
-                                '.part1.rar',
-                                '.part01.rar',
-                                '.part001.rar',
-                                '.part0001.rar',
-                            )
-                            if (
-                                file.endswith(".rar") and "part" not in file
-                            ) or file.endswith(suffixes):
+                            if re.search(r'\.part0*1.rar$', file) or re.search(r'\.7z.0*1$', file) \
+                               or (filee.endswith(".rar") and not re.search(r'\.part\d+.rar$', file)) \
+                               or re.search(r'\.zip.0*1$', file):
                                 m_path = os.path.join(dirpath, file)
                                 if pswd is not None:
                                     result = subprocess.run(
@@ -171,11 +164,8 @@ class MirrorListener(listeners.MirrorListeners):
                                     LOGGER.warning("Unable to extract archive!")
                                 break
                         for file in files:
-                            if (
-                                file.endswith(".rar")
-                                or fnmatch(file, "*.r[0-9]")
-                                or fnmatch(file, "*.r[0-9]*")
-                            ):
+                            if file.endswith(".rar") or re.search(r'\.r\d+$', file) \
+                               or re.search(r'\.7z.\d+$', file) or re.search(r'\.zip.\d+$', file):
                                 del_path = os.path.join(dirpath, file)
                                 os.remove(del_path)
                     path = f'{DOWNLOAD_DIR}{self.uid}/{name}'
@@ -518,8 +508,8 @@ def _mirror(
             sendMessage(res, bot, update)
             return
         if TAR_UNZIP_LIMIT is not None:
-            result = bot_utils.check_limit(size, TAR_UNZIP_LIMIT)
-            if result:
+            LOGGER.info('Memeriksa Ukuran File/Folder...')
+            if size > TAR_UNZIP_LIMIT * 1024**3:
                 msg = f'Gagal, batas Tar/Unzip adalah {TAR_UNZIP_LIMIT}.\nUkuran File/Folder Anda adalah {get_readable_file_size(size)}. '
                 sendMessage(msg, bot, update)
                 return