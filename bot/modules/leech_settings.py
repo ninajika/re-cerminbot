19439924e89d6f53b08746cc0326e4380b518e5d
narima <private.nafi2016@gmail.com>
2021-10-14 19:25:15 +0700

    refactoring
    some cleanup code

--- bot/modules/leech_settings.py
+++ bot/modules/leech_settings.py
@@ -7,14 +7,26 @@ import os
 import threading
 
 from PIL import Image
-from telegram.ext import CommandHandler, CallbackQueryHandler
 from telegram import InlineKeyboardMarkup
+from telegram.ext import CallbackQueryHandler, CommandHandler
 
-from bot import AS_DOC_USERS, AS_MEDIA_USERS, dispatcher, AS_DOCUMENT, app, AUTO_DELETE_MESSAGE_DURATION
-from bot.helper.telegram_helper.message_utils import sendMessage, sendMarkup, editMessage, auto_delete_message
-from bot.helper.telegram_helper.filters import CustomFilters
-from bot.helper.telegram_helper.bot_commands import BotCommands
+from bot import (
+    AS_DOC_USERS,
+    AS_DOCUMENT,
+    AS_MEDIA_USERS,
+    AUTO_DELETE_MESSAGE_DURATION,
+    app,
+    dispatcher,
+)
 from bot.helper.telegram_helper import button_build
+from bot.helper.telegram_helper.bot_commands import BotCommands
+from bot.helper.telegram_helper.filters import CustomFilters
+from bot.helper.telegram_helper.message_utils import (
+    auto_delete_message,
+    editMessage,
+    sendMarkup,
+    sendMessage,
+)
 
 
 def getleechinfo(from_user):
@@ -22,41 +34,43 @@ def getleechinfo(from_user):
     name = from_user.full_name
     buttons = button_build.ButtonMaker()
     thumbpath = f"Thumbnails/{user_id}.jpg"
-    if (
-            user_id in AS_DOC_USERS
-            or user_id not in AS_MEDIA_USERS
-            and AS_DOCUMENT
-    ):
+    if user_id in AS_DOC_USERS or user_id not in AS_MEDIA_USERS and AS_DOCUMENT:
         ltype = "DOCUMENT"
         buttons.sbutton("Send As Media", f"med {user_id}")
     else:
         ltype = "MEDIA"
         buttons.sbutton("Send As Document", f"doc {user_id}")
-    
+
     if os.path.exists(thumbpath):
-        thumbmsg =  "Exists"
+        thumbmsg = "Exists"
         buttons.sbutton("Delete Thumbnail", f"thumb {user_id}")
     else:
-        thumbmsg =  "Not Exists"
-    
+        thumbmsg = "Not Exists"
+
     if AUTO_DELETE_MESSAGE_DURATION == -1:
         buttons.sbutton("Close", f"closeset {user_id}")
-    
+
     button = InlineKeyboardMarkup(buttons.build_menu(1))
 
-    text = f"<u>Leech Setting Of <a href='tg://user?id={user_id}'>{name}</a></u>\n"\
-           f"Leech Type <b>{ltype}</b>\n"\
-           f"Custom Thumbnail <b>{thumbmsg}</b>"
+    text = (
+        f"<u>Leech Setting Of <a href='tg://user?id={user_id}'>{name}</a></u>\n"
+        f"Leech Type <b>{ltype}</b>\n"
+        f"Custom Thumbnail <b>{thumbmsg}</b>"
+    )
     return text, button
+
 
 def editLeechType(message, query):
     msg, button = getleechinfo(query.from_user)
     editMessage(msg, message, button)
 
+
 def leechSet(update, context):
     msg, button = getleechinfo(update.message.from_user)
     choose_msg = sendMarkup(msg, context.bot, update, button)
-    threading.Thread(target=auto_delete_message, args=(context.bot, update.message, choose_msg)).start()
+    threading.Thread(
+        target=auto_delete_message, args=(context.bot, update.message, choose_msg)
+    ).start()
 
 
 def setLeechType(update, context):
@@ -68,11 +82,7 @@ def setLeechType(update, context):
     if user_id != int(data[1]):
         query.answer(text="Not Yours!", show_alert=True)
     elif data[0] == "doc":
-        if (
-                user_id in AS_DOC_USERS
-                or user_id not in AS_MEDIA_USERS
-                and AS_DOCUMENT
-        ):
+        if user_id in AS_DOC_USERS or user_id not in AS_MEDIA_USERS and AS_DOCUMENT:
             query.answer(text="Already As Document!", show_alert=True)
         elif user_id in AS_MEDIA_USERS:
             AS_MEDIA_USERS.remove(user_id)
@@ -118,28 +128,42 @@ def setThumb(update, context):
         path = "Thumbnails/"
         if not os.path.isdir(path):
             os.mkdir(path)
-        photo_msg = app.get_messages(update.message.chat.id, reply_to_message_ids=update.message.message_id)
+        photo_msg = app.get_messages(
+            update.message.chat.id, reply_to_message_ids=update.message.message_id
+        )
         photo_dir = app.download_media(photo_msg, file_name=path)
         des_dir = os.path.join(path, str(user_id) + ".jpg")
         img = Image.open(photo_dir)
         img.thumbnail((480, 320))
         img.save(des_dir, "JPEG")
         os.remove(photo_dir)
-        sendMessage(f"Custom thumbnail saved for <a href='tg://user?id={user_id}'>{update.message.from_user.full_name}</a> .", context.bot, update)
+        sendMessage(
+            f"Custom thumbnail saved for <a href='tg://user?id={user_id}'>{update.message.from_user.full_name}</a> .",
+            context.bot,
+            update,
+        )
     else:
         sendMessage("Reply to a photo to save custom thumbnail.", context.bot, update)
 
 
-leech_set_handler = CommandHandler(BotCommands.LeechSetCommand, leechSet,
-                                   filters=CustomFilters.authorized_chat | CustomFilters.authorized_user,
-                                   run_async=True)
-set_thumbnail_handler = CommandHandler(BotCommands.SetThumbCommand, setThumb,
-                                       filters=CustomFilters.authorized_chat | CustomFilters.authorized_user,
-                                       run_async=True)
+leech_set_handler = CommandHandler(
+    BotCommands.LeechSetCommand,
+    leechSet,
+    filters=CustomFilters.authorized_chat | CustomFilters.authorized_user,
+    run_async=True,
+)
+set_thumbnail_handler = CommandHandler(
+    BotCommands.SetThumbCommand,
+    setThumb,
+    filters=CustomFilters.authorized_chat | CustomFilters.authorized_user,
+    run_async=True,
+)
 as_doc_handler = CallbackQueryHandler(setLeechType, pattern="doc", run_async=True)
 as_media_handler = CallbackQueryHandler(setLeechType, pattern="med", run_async=True)
 del_thumb_handler = CallbackQueryHandler(setLeechType, pattern="thumb", run_async=True)
-close_set_handler = CallbackQueryHandler(setLeechType, pattern="closeset", run_async=True)
+close_set_handler = CallbackQueryHandler(
+    setLeechType, pattern="closeset", run_async=True
+)
 dispatcher.add_handler(leech_set_handler)
 dispatcher.add_handler(as_doc_handler)
 dispatcher.add_handler(as_media_handler)
